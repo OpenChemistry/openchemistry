9b0da3188c9903b82ecf744206cdfd0136fa751f
David Cole <david.cole@kitware.com>
2012-04-18 11:14:25 -0400

    Update CDash URL: we have a new dashboard server!

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -8,6 +8,6 @@ set(CTEST_PROJECT_NAME "OpenChemistry")
 set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
 
 set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "trunk.cdash.org")
+set(CTEST_DROP_SITE "cdash.openchemistry.org")
 set(CTEST_DROP_LOCATION "/submit.php?project=OpenChemistry")
 set(CTEST_DROP_SITE_CDASH TRUE)
