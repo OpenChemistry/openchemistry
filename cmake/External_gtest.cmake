04f656f7dd4178c5b12f337a9aba36509863472c
David Cole <david.cole@kitware.com>
2012-11-08 14:21:54 -0500

    Add GTest to the SuperBuild
    Change-Id: I2d88becd600fd60dc9fdb378d39ec8b80863db6d

---
+++ cmake/External_gtest.cmake
@@ -0,0 +1,21 @@
+get_filename_component(_self_dir ${CMAKE_CURRENT_LIST_FILE} PATH)
+
+configure_file(
+  "${_self_dir}/gtest.CMakeLists.txt.in"
+  "${CMAKE_CURRENT_BINARY_DIR}/gtest.CMakeLists.txt"
+  @ONLY
+  )
+
+ExternalProject_Add(gtest
+  DOWNLOAD_DIR ${download_dir}
+  URL ${gtest_url}
+  URL_MD5 ${gtest_md5}
+  BINARY_DIR "${CMAKE_CURRENT_BINARY_DIR}/gtest"
+  INSTALL_DIR "${OpenChemistry_INSTALL_PREFIX}"
+  PATCH_COMMAND ${CMAKE_COMMAND} -E copy_if_different
+    "${CMAKE_CURRENT_BINARY_DIR}/gtest.CMakeLists.txt"
+    "<SOURCE_DIR>/CMakeLists.txt"
+  CMAKE_ARGS
+    -DBUILD_SHARED_LIBS:BOOL=${BUILD_SHARED_LIBS}
+    -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
+    )
