35c264791c24b4bb3e7295efe4d85284baa86c64
Marcus D. Hanwell <marcus.hanwell@kitware.com>
2013-01-11 17:39:06 -0500

    Added QtTesting to the Open Chemistry superbuild
    This will be used in Avogadro, MongoChem and MoleQueue for testing
    automation.
    
    Change-Id: Id04f1e7df4753f10b280f387f5c3a1e767688874

---
+++ cmake/External_qttesting.cmake
@@ -0,0 +1,11 @@
+# Pick out the modules used in MongoChem and AvogadroLibs only.
+set(_source "${CMAKE_CURRENT_SOURCE_DIR}/qttesting")
+set(_build "${CMAKE_CURRENT_BINARY_DIR}/qttesting")
+
+ExternalProject_Add(qttesting
+  SOURCE_DIR ${_source}
+  BINARY_DIR ${_build}
+  CMAKE_ARGS
+    ${OpenChemistry_DEFAULT_ARGS}
+    -DQT_TESTING_INSTALL_DEVELOPMENT:BOOL=ON
+  )
