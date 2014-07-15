a1d681ebea5670b9ef8a45b6bd078d58c5bff15d
Marcus D. Hanwell <marcus.hanwell@kitware.com>
2014-07-15 12:00:33 -0400

    Added more conditionals for kdsoap/UIT in MoleQueue
    Only build KDSoap if UIT integration was enabled in MoleQueue.
    
    Change-Id: I951f1eb29091ce7457bebb134730a30d311cc7b6

--- cmake/External_molequeue.cmake
+++ cmake/External_molequeue.cmake
@@ -1,12 +1,23 @@
 set(molequeue_source "${CMAKE_CURRENT_SOURCE_DIR}/molequeue")
 set(molequeue_build "${CMAKE_CURRENT_BINARY_DIR}/molequeue")
 
+unset(_deps)
+if(BUILD_MOLEQUEUE_UIT)
+  set(_deps "kdsoap")
+  set(_uit "ON")
+else()
+  set(_uit "OFF")
+endif()
+
 ExternalProject_Add(molequeue
   SOURCE_DIR ${molequeue_source}
   BINARY_DIR ${molequeue_build}
   CMAKE_CACHE_ARGS
     ${OpenChemistry_DEFAULT_ARGS}
     ${OpenChemistry_THIRDPARTYLIBS_ARGS}
+    -DMoleQueue_USE_EZHPC_UIT:BOOL=${_uit}
+  DEPENDS
+    ${_deps}
   )
 
 if(FORCE_STEP)
