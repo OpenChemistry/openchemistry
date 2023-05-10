dc6cf7fc22335c2a012afa889fa26d77ec37f643
Geoff Hutchison <geoff.hutchison@gmail.com>
2023-05-10 03:50:03 +0200

    Add RPATH to ensure eht_bind works on Mac
    Signed-off-by: Geoff Hutchison <geoff.hutchison@gmail.com>

--- cmake/External_yaehmop.cmake
+++ cmake/External_yaehmop.cmake
@@ -8,6 +8,9 @@ ExternalProject_Add(yaehmop
   SOURCE_DIR ${_source}
   BINARY_DIR ${_build}
   INSTALL_DIR ${_install}
+  PATCH_COMMAND ${CMAKE_COMMAND} -E copy_if_different
+  "${_self_dir}/tightbind.CMakeLists.txt"
+  "<SOURCE_DIR>/CMakeLists.txt"
   CMAKE_CACHE_ARGS
     ${OpenChemistry_DEFAULT_ARGS}
     -DBIND_EXE_NAME:FILEPATH=eht_bind
