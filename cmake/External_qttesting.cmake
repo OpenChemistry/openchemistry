01096f42f4225d1895aca345f4f0e0e39ae55f75
Geoff Hutchison <geoff.hutchison@gmail.com>
2025-11-30 09:59:17 -0500

    Update default visibility for QtTesting
    Hopefully fixes compile issues on Windows
    
    Signed-off-by: Geoff Hutchison <geoff.hutchison@gmail.com>

--- cmake/External_qttesting.cmake
+++ cmake/External_qttesting.cmake
@@ -2,7 +2,8 @@
 set(_source "${CMAKE_CURRENT_SOURCE_DIR}/qttesting")
 set(_build "${CMAKE_CURRENT_BINARY_DIR}/qttesting")
 
-
+set(CMAKE_CXX_VISIBILITY_PRESET "default")
+set(CMAKE_VISIBILITY_INLINES_HIDDEN 0)
 
 ExternalProject_Add(qttesting
   SOURCE_DIR ${_source}
